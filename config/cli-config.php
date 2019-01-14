244a5863156221e737795396062c17dddd615e39
fenric <afenric@gmail.com>
2019-01-14 19:23:42 +0300

    Update cli-config.php

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
 chdir(dirname(__DIR__));
-
 require_once 'vendor/autoload.php';
-
 $container = require 'config/container.php';
 
-$entityManager = $container->get(Doctrine\ORM\EntityManager::class);
-
-return Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+$entityManager = $container->get(EntityManager::class);
+return ConsoleRunner::createHelperSet($entityManager);
