a5d684200ba7ccd3929c65b4066bdc5330edce23
fenric <afenric@gmail.com>
2019-01-14 19:23:49 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php declare(strict_types=1);
 
+use App\Http\App;
+use Sunrise\Http\Factory\ServerRequestFactory;
+
 chdir(dirname(__DIR__));
-
 require_once 'vendor/autoload.php';
-
 $container = require 'config/container.php';
 
-$application = $container->get(App\Http\App::class);
-
-$application->run(Sunrise\Http\Factory\ServerRequestFactory::fromGlobals());
+$application = $container->get(App::class);
+$application->run(ServerRequestFactory::fromGlobals());
 
 exit(1);
