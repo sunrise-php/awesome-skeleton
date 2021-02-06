6bcb97524b49d6d162053b900bf309b83ac206ac
Anatoly Nekhay <afenric@gmail.com>
2021-02-06 13:35:10 +0500

    minor changes

--- config/container.php
+++ config/container.php
@@ -4,7 +4,7 @@ use DI\Container;
 use DI\ContainerBuilder;
 
 return (function () : Container {
-    $env = getenv('APP_ENV') ?: 'dev';
+    $env = $_ENV['APP_ENV'] ?? 'dev';
 
     $builder = new ContainerBuilder();
     $builder->useAutowiring(true);
@@ -15,7 +15,6 @@ return (function () : Container {
         ...glob(__DIR__ . '/definitions/*.php'),
         ...glob(__DIR__ . '/definitions/*.php.' . $env),
         ...glob(__DIR__ . '/definitions/*.php.local'),
-
         // bundle definitions...
         ...glob(__DIR__ . '/bundles/*/definitions/*.php'),
         ...glob(__DIR__ . '/bundles/*/definitions/*.php.' . $env),
