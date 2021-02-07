265bd4fc1b691543b5cd55e9aa26a52c4cc4cfbc
Anatoly Nekhay <afenric@gmail.com>
2021-02-07 05:49:21 +0500

    minor changes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -7,10 +7,9 @@ chdir(__DIR__ . '/..');
 setlocale(LC_ALL, 'C.UTF-8');
 
 // enables strict development mode...
-// good code shouldn't contain notices, warnings and etc.
 set_error_handler(function ($severity, $message, $file, $line) {
     throw new ErrorException($message, 0, $severity, $file, $line);
 });
 
-// loads environment variables from the `.env` file...
+// loads environment variables...
 (new Dotenv)->usePutenv()->load(__DIR__ . '/../.env');
