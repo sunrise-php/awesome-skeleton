9113f5e0e1e8a3d0459d4aaab6b92a1445f293aa
Anatoly Nekhay <afenric@gmail.com>
2021-02-07 06:31:31 +0500

    minor changes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,6 @@
 use Symfony\Component\Dotenv\Dotenv;
 
 chdir(__DIR__ . '/..');
-setlocale(LC_ALL, 'C.UTF-8');
 
 // enables strict development mode...
 set_error_handler(function ($severity, $message, $file, $line) {
