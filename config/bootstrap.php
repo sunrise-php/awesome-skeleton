31ceed3554ba01b931372c324bcfdfc72ecf39db
Anatoly Nekhay <afenric@gmail.com>
2021-02-07 05:40:50 +0500

    minor changes

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,8 +12,5 @@ set_error_handler(function ($severity, $message, $file, $line) {
     throw new ErrorException($message, 0, $severity, $file, $line);
 });
 
-// enables annotations autoload...
-/** @scrutinizer ignore-deprecated */ AnnotationRegistry::registerLoader('class_exists');
-
 // loads environment variables from the `.env` file...
 (new Dotenv)->usePutenv()->load(__DIR__ . '/../.env');
