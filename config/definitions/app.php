56110f921e821174b07b6cf86f3028461c34689c
Anatoly Nekhay <afenric@gmail.com>
2025-02-23 05:17:21 +0100

    v4

--- config/definitions/app.php
+++ config/definitions/app.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 use App\Dictionary\DomainContracts;
+use Sunrise\Http\Router\Command\RouterClearDescriptorsCacheCommand;
+use Sunrise\Http\Router\Command\RouterListRoutesCommand;
+use Sunrise\Http\Router\OpenApi\Command\RouterOpenApiBuildDocumentCommand;
 
+use function DI\autowire;
 use function DI\env;
 
 return [
@@ -15,4 +19,10 @@ return [
     'app.input_timestamp_format' => DomainContracts::INPUT_TIMESTAMP_FORMAT,
     'app.output_timestamp_format' => DomainContracts::OUTPUT_TIMESTAMP_FORMAT,
     'app.timezone' => DomainContracts::TIMEZONE,
+
+    'app.commands' => [
+        autowire(RouterClearDescriptorsCacheCommand::class),
+        autowire(RouterListRoutesCommand::class),
+        autowire(RouterOpenApiBuildDocumentCommand::class),
+    ],
 ];
